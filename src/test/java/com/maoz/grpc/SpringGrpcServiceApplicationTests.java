2180157c5ef85aecd162f25082a380639c14c0f1
fhluky <fhluky@gmail.com>
2019-04-04 15:05:12 +0700

    finished

--- src/test/java/com/maoz/grpc/SpringGrpcServiceApplicationTests.java
+++ src/test/java/com/maoz/grpc/SpringGrpcServiceApplicationTests.java
@@ -1,24 +1,16 @@
 package com.maoz.grpc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.maoz.grpc.service.HelloWorldClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringGrpcServiceApplicationTests {
 
-	@Autowired
-	private HelloWorldClient helloWorldClient;
 
 	@Test
 	public void testSayHello() {
-		assertThat(helloWorldClient.sayHello("John", "Doe")).isEqualTo("Hello John Doe!");
 	}
 }
