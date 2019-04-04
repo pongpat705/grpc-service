7bb8d943136e88c95704326bd9a0357d7b41440d
fhluky <fhluky@gmail.com>
2019-04-04 11:53:31 +0700

    first commit

---
+++ src/main/java/com/maoz/grpc/SpringGrpcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maoz.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrpcServiceApplication.class, args);
+	}
+
+}
